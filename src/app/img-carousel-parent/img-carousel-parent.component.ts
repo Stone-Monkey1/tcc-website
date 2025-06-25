0e1646f3f0dc8d6491a1c779ea220d4956d0e6ec
Alec Powell <alecpow@gmail.com>
2025-06-25 15:38:03 -0400

    Update 6/25/25: Added 2 new projects, changed name of the company, changed favicon

--- src/app/img-carousel-parent/img-carousel-parent.component.ts
+++ src/app/img-carousel-parent/img-carousel-parent.component.ts
@@ -31,13 +31,32 @@ export class ImgCarouselParentComponent {
       description: 'Enlarged existing screen porch, installed much less intrusive cable rail system and installed new screening.',
       customClass: 'carousel-2',
     },
+    // {
+    //   carouselItems: [
+    //     { backgroundClass: 'siding-replacement-before', title: 'Before' },
+    //     { backgroundClass: 'siding-replacement-after', title: 'After' },
+    //   ],
+    //   description: 'Siding replacement for a more modern look.',
+    //   customClass: 'carousel-3',
+    // },
     {
       carouselItems: [
-        { backgroundClass: 'siding-replacement-before', title: 'Before' },
-        { backgroundClass: 'siding-replacement-after', title: 'After' },
+        { backgroundClass: 'gordons-1', title: 'Completed' },
+        { backgroundClass: 'gordons-2', title: 'Completed' },
+        { backgroundClass: 'gordons-3', title: 'Completed' },
+        { backgroundClass: 'gordons-4', title: 'Completed' },
       ],
-      description: 'Siding replacement for a more modern look.',
-      customClass: 'carousel-3',
+      description: 'From an open lot to a fully constructed detached garage, this project was built from the ground up. We handled everything from the foundation and framing to roofing, siding, and final finishes—creating a functional and durable space that blends seamlessly with the property.',
+      customClass: 'carousel-4',
+    },
+    {
+      carouselItems: [
+        { backgroundClass: 's-laurel-1', title: 'Before' },
+        { backgroundClass: 's-laurel-3', title: 'After' },
+        { backgroundClass: 's-laurel-2', title: 'After' },
+      ],
+      description: `We gave this home's entryway a fresh new look by scraping down the worn paint and refinishing the front pillars with a crisp, clean coat. The result is a polished, updated appearance that enhances the home’s curb appeal and protects the structure for years to come.`,
+      customClass: 'carousel-5',
     },
   ];
 }
